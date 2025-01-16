f4531835407abd302ffb451b3f9407b536f05a74
hemant069 <hemantprajapati20500@gmail.com>
2025-01-16 21:58:01 +0530

    .env local file is added

--- connect.js
+++ connect.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
-
+const dotenv = require("dotenv");
+dotenv.config();
 const connectDB = async () => {
+  const URL = process.env.MONGO_URL;
   try {
-    mongoose.connect("mongodb://127.0.0.1:27017/url-shortner");
+    mongoose.connect(URL);
   } catch (error) {
     console.error("something wrong with DB");
   }
