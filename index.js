79ca21d6aa3c6f3d12608ab61e185582e8be0fa1
hemant069 <hemantprajapati20500@gmail.com>
2025-01-18 23:26:37 +0530

    ejs file is added

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const connectDB = require("./connect");
 const urlShortRoute = require("./routes/index");
-
+// server side rendering using EJS
+const ejs = require("ejs");
+const path = require("path");
+const urlModel = require("./models/url-shorter.model");
 const app = express();
 const PORT = 8000;
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.get("/test", async (req, res) => {
+  const urls = await urlModel.find();
+
+  return res.render("Home", { urls });
+});
 
 // DB call is here
 
